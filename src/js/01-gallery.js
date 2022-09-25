f1349c44deba4861be6184059655d547f19bfe90
Denisiimo <denischorny22@gmail.com>
2022-09-25 22:19:37 +0100

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+const itemCreate = cardCreate(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', itemCreate);
 
+const simple = new SimpleLightbox(".gallery a", { captionDelay: 250, CaptionsData: "alt" });
+
+function cardCreate(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`
+    }).join('');
+}
 console.log(galleryItems);
